a1f37907f73202615b26f0ec67cac976d46d796b
jguiver <jessica.guiver@slcc.edu>
2019-05-08 17:58:39 -0600

    gitActivity

---
+++ Documents/DevProjects/C#/Travel/Travel/Trip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Travel
+{
+    public class EmptyClass
+    {
+        string fName;
+        string lName;
+        string city;
+        string state;
+
+        public EmptyClass(string fNam, string lNam, string c, string s)
+        {
+            fName = fNam;
+            lName = lNam;
+            city = c;
+            state = s;
+        }
+
+        public override string ToString()
+        {
+            return $"fName + lName + city + state";
+        }
+
+    }
+}
+
